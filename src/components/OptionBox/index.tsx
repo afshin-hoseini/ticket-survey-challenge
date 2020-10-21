bb9b12b6ca8eacc25a27756c76c7cfb6b9b6b145
Afshin <afshin.hoseini@gmail.com>
2020-10-21 19:09:44 +0330

    Implements option box atom component

---
+++ src/components/OptionBox/index.tsx
@@ -0,0 +1,16 @@
+import React, { FC, useMemo } from 'react';
+import { OptionBoxComponentProps } from './@types';
+import './styles.css';
+
+export const OptionBox: FC<OptionBoxComponentProps> = ({ className, ...props }) => {
+  return useMemo(
+    () => (
+      <select {...props} className={`--custom-select ${className || ''}`}>
+        {(props.options || []).map((item) => (
+          <option value={item.value}>{item.label}</option>
+        ))}
+      </select>
+    ),
+    [className, props]
+  );
+};
