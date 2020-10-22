3ae25054cce5e7b74fbd6050bdb268bc593a4339
Afshin <afshin.hoseini@gmail.com>
2020-10-22 16:11:02 +0330

    Implemented ticket data source.

---
+++ src/dataSource/tickets/ticket1.ts
@@ -0,0 +1,30 @@
+import { RouteType, Ticket } from 'src/@types';
+
+export default {
+  code: 'MQX12',
+  id: 'T1',
+  title: 'Short stay with no kids',
+  routes: [
+    {
+      type: RouteType.bike,
+      start: { latitude: 0.345, longitude: 0.234, name: 'street 1st' },
+      end: { latitude: 0.345, longitude: 0.234, name: 'street 2nd' },
+      startTime: new Date(2020, 5, 6, 10, 20),
+      endTime: new Date(2020, 5, 6, 10, 50),
+    },
+    {
+      type: RouteType.walk,
+      start: { latitude: 0.345, longitude: 0.234, name: 'Square 1' },
+      end: { latitude: 0.345, longitude: 0.234, name: 'Intersection 2' },
+      startTime: new Date(2020, 5, 6, 10, 50),
+      endTime: new Date(2020, 5, 6, 11, 30),
+    },
+    {
+      type: RouteType.bus,
+      start: { latitude: 0.345, longitude: 0.234, name: 'Station 25th' },
+      end: { latitude: 0.345, longitude: 0.234, name: 'Amadeus Park' },
+      startTime: new Date(2020, 5, 6, 11, 30),
+      endTime: new Date(2020, 5, 6, 11, 55),
+    },
+  ],
+} as Ticket;
